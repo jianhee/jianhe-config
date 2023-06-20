502b2fc0493c14bc8c495549641157eebc94650e
jianhe <jianhee@qq.com>
2023-06-20 22:39:57 +0800

    update

--- eslint/vue3.js
+++ eslint/vue3.js
@@ -7,8 +7,9 @@ module.exports = {
   // 可共享配置
   extends: [
     // Vue 插件提供的配置
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     // 本地配置
-    './_base.js'
+    './_base.js',
+    './vue.js'
   ]
 };
